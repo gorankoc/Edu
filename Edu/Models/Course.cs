c99cd621b9ee71807e24e719af5de74ca07097e9
Goran <goooran.k@gmail.com>
2018-09-08 15:13:45 +0200

    Defaut

---
+++ Edu/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Edu.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int DurationInHours { get; set; }
+    }
+}
---
+++ Edu/Models/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Edu.Models
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public int OIB { get; set; }
+    }
+}
